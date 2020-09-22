d2efce85a4226ae76856307b8dc2de3112eeda28
s3cret <j.s3cret@gmail.com>
2020-09-22 12:04:17 +0800

    Solved for Printer Queue UVa 12100

---
+++ src/Chapter 5/exercise/PriterQueue-UVa12100.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <string.h>
+
+#ifdef LOCAL
+#include <My_Util.h>
+#endif
+
+int Q[1001];
+
+int main()
+{
+#ifdef LOCAL
+    char* filename;
+    get_input_filename(filename, __FILE__);
+    freopen(filename, "r", stdin);
+    get_output_filename(filename);
+    freopen(filename, "w", stdout);
+#endif
+    int K;
+    scanf("%d", &K);
+    for (int k = 0; k < K; k++)
+    {
+        memset(Q, 0, sizeof(Q));
+        int n, m;
+        scanf("%d%d", &n, &m);
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &Q[i]);
+        }
+
+        // solution
+        int head = 0, tail = n - 1, ans = 0;
+        while (head < tail)
+        {
+            int J = Q[head];
+            if (J == 0)
+            {
+                head++;
+                continue;
+            }
+
+            ans++;
+            // find max_i
+            int max_i = head;
+            for (int i = head; i <= tail; i++)
+            {
+                if (Q[i] > Q[max_i])
+                {
+                    max_i = i;
+                }
+            }
+
+            if (max_i == m)
+            {
+                break;
+            }
+
+            if (head != max_i)
+            {
+                ans--;
+                Q[tail + 1] = J;
+                // Q[head] = Q[max_i];
+                // Q[max_i] = 0;
+                tail++;
+                if (head == m)
+                {
+                    m = tail;
+                }
+            }
+            head++;
+        }
+
+        if (head == tail)
+        {
+            ans++;
+        }
+
+        printf("%d\n", ans);
+    }
+
+    return 0;
+}
